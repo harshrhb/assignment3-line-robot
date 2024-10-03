c8a868594888b3a87e948121645113ff69d0e855
Zachary Bouda <zacharybouda@gmail.com>
2024-10-04 11:13:58 +1300

    Create main.cpp
    Basic radio transmission. (Receive).

---
+++ source/radio/receive/main.cpp
@@ -0,0 +1,24 @@
+#include "MicroBit.h"
+
+MicroBit uBit;
+
+void onData(MicroBitEvent){
+    ManagedString s = uBit.radio.datagram.recv();
+
+    if (s == "1")
+        uBit.display.print("A");
+
+    if (s == "2")
+        uBit.display.print("B");
+}
+
+int main(){
+    // Initialise the micro:bit runtime.
+    uBit.init();
+
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+    uBit.radio.enable();
+
+    while(1)
+        uBit.sleep(1000);
+}
